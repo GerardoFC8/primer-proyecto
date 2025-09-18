13915ce99153d14885dc761a630a85ef614703c8
GerardoFC8 <gerardofranco366@gmail.com>
2025-09-18 15:53:46 -0500

    cambios combinados en main

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,6 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         
         PRIMER CAMBIO DESDE GITHUB PARA PROBAR EL PULL AGREGANDO LINEAS
-        PRIMER CAMBIO DESDE GITHUB PARA PROBAR EL PU
 
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-pie-icon lucide-chart-pie"><path d="M21 12c.552 0 1.005-.449.95-.998a10 10 0 0 0-8.953-8.951c-.55-.055-.998.398-.998.95v8a1 1 0 0 0 1 1z"/><path d="M21.21 15.89A10 10 0 1 1 8 2.83"/></svg>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
